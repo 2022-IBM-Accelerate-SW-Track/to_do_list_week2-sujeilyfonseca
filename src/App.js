82bd508fa7a635532fd40fe4567f03a5d0d58fec
Sujeily Fonseca <sujeily.fonseca@ibm.com>
2022-06-19 16:20:24 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavbarComp from './component/navigation/NavbarComp';
 
+// Comment to trigger the automated test execution
 function App() {
   return (
     <div className="App">
